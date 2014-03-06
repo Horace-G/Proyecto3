97dc5ce7783f65278c87511a47da5491d45c4a96
Horace-G <horacio.galdamez@unitec.edu>
2014-03-05 19:17:57 -0600

    Agregando la clase Nodo al proyecto

---
+++ src/proyecto3_vuelos/Node.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto3_vuelos;
+
+/**
+ *
+ * @author Horacio Galdamez
+ */
+
+/*
+ * clase nodo de los que serán utilizados en la clase list, la cual es una lista de nodos
+ */
+public class Node {
+    public Object o;
+    public Node next;
+    
+    public Node(){
+        this.o=null;
+        this.next=null;
+    }//fin el constructor 
+}//fin de la clase
